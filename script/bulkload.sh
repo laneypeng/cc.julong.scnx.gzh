b736594972bad3a0ea337e410e4fcb5f38a40b66
zhangfeng <hf200012@163.com>
2015-11-24 21:23:21 +0800

    添加执行导入的shell脚本

---
+++ script/bulkload.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+inputPath=$1
+outputPath=$2
+tableName=$3
+echo "inputPath  == "$1
+echo "outputPath == "$2
+echo "tableName  == "$3
+
+for i in `ls /weblogic/domains/1213/msimport/war/msimport/WEB-INF/lib/*.jar`
+do
+        LIBJARS=$i,$LIBJARS
+        HADOOPCLASSPATH=$i:$HADOOPCLASSPATH
+done
+export CLASSPATH=$CLASSPATH:$HADOOPCLASSPATH
+export JAVA_LIBRARY_PATH=$JAVA_LIBRARY_PATH:/weblogic/tdhcli/base/hadoop/lib/native:$LIBJARS
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/weblogic/tdhcli/base/hadoop/lib/native:$LIBJARS
+
+echo "JAVA_LIBRARY_PATH=======:$JAVA_LIBRARY_PATH "
+echo "LD_LIBRARY_PATH=========:$LD_LIBRARY_PATH"
+
+echo "===========start Driver==========="
+java -Djava.security.krb5.conf="/etc/krb5.conf"  com.apache.hbase.bulkimport.Driver  $inputPath $outputPath $tableName $day
